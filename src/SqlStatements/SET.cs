32be2378b939244a6c16c764fbe4d2012218c935
Leonid Petrov1 <leonid_petrov1@epam.com>
2022-01-09 00:53:02 +0400

    Add sql SET statements

---
+++ src/SqlStatements/SET.cs
@@ -0,0 +1,11 @@
+﻿namespace SqlQuery.WebApi.SqlStatements
+{
+    public static class SET
+    {
+        public const string NOEXEC_ON = "SET NOEXEC ON;";
+        public const string NOEXEC_OFF = "SET NOEXEC OFF;";
+
+        public const string PARSEONLY_ON = "SET PARSEONLY ON;";
+        public const string PARSEONLY_OFF = "SET PARSEONLY OFF;";
+    }
+}
