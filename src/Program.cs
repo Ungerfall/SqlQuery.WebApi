6f16b7866c871237cb3353ce48156f283161dd52
Leonid Petrov1 <leonid_petrov1@epam.com>
2022-01-09 22:06:26 +0400

    Change connection string initilization according to Azure naming conventions

--- src/Program.cs
+++ src/Program.cs
@@ -23,10 +23,16 @@ namespace src
 
         public static IDbConnection DbConnectionFactoryMethod(IServiceProvider _)
         {
-            string? connectionString = Environment.GetEnvironmentVariable(
-                $"ConnectionStrings:Default",
-                EnvironmentVariableTarget.Process);
+            string? connectionString = GetSqlAzureConnectionString("Default");
             return new SqlConnection(connectionString);
+        }
+
+        public static string? GetSqlAzureConnectionString(string name)
+        {
+            string? conStr = Environment.GetEnvironmentVariable($"ConnectionStrings:{name}", EnvironmentVariableTarget.Process);
+            if (string.IsNullOrEmpty(conStr)) // Azure Functions App Service naming convention
+                conStr = Environment.GetEnvironmentVariable($"SQLAZURECONNSTR_{name}", EnvironmentVariableTarget.Process);
+            return conStr;
         }
     }
 }
